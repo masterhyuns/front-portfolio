b4d9d596fbc869829c4588f74b55560787c1a5aa
masterhyuns <masterhyuns@naver.com>
2025-04-14 00:53:40 +0900

    [FIX] UI Components 생성 Button, Modal 완료

--- apps/portfolio/app/routes/_landing.components._index/index.tsx
+++ apps/portfolio/app/routes/_landing.components._index/index.tsx
@@ -1,4 +1,15 @@
+import { SourceLink } from '@portfolio/ui';
+
 const ComponentsPage = () => {
-  return <div>Projects Page</div>;
+  return (
+    <div>
+      <SourceLink href={'https://github.com/masterhyuns/front-portfolio'} />
+      <img
+        src="/img.png"
+        alt="프로젝트 아키텍처 다이어그램"
+        style={{ width: '50%', marginBottom: '2rem', borderRadius: '8px' }}
+      />
+    </div>
+  );
 };
 export default ComponentsPage;
